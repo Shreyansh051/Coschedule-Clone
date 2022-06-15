1c5a2d55d79d5e056466ead1d97c08ed6faa6a32
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-06-16 01:02:55 +0530

    createcalendar with css

--- coschedule/src/pages/CreateCalendar.jsx
+++ coschedule/src/pages/CreateCalendar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from "./getademo.module.css"
+import styles from "./createcalendar.module.css"
 
 const CreateCalendar = () => {
   return (
