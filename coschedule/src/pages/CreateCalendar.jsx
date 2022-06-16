3e9c7d327f7ead47008e794c9bacdde8436b290a
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-06-17 01:47:18 +0530

    media queries

--- coschedule/src/pages/CreateCalendar.jsx
+++ coschedule/src/pages/CreateCalendar.jsx
@@ -1,60 +1,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from "./createcalendar.module.css"
+import "../styles/createcalendar.css"
 
 const CreateCalendar = () => {
   return (
     <div>
-        <div className={styles.upperDiv}>
-            {/* <Link to="/" ><img src="https://coschedule.com/img/cos-logo-full-color.svg" className={styles.cosLogo} alt="CoSchedule Logo" /></Link>
+        <div className="upperDiv">
+            {/* <Link to="/" ><img src="https://coschedule.com/img/cos-logo-full-color.svg" className="cosLogo} alt="CoSchedule Logo" /></Link>
             <br /> */}
-            <div className={styles.cosDemoTitle}>
-                <p className={styles.cosTitle}>Create My Marketing Calendar</p>
-                <p className={styles.organizeCoschedule}>Free for life. No credit card required.</p>
+            <div className="cosDemoTitle">
+                <p className="cosTitle">Create My Marketing Calendar</p>
+                <p className="organizeCoschedule">Free for life. No credit card required.</p>
             </div>
         </div>
 
-        <div className= {styles.formDiv}>
+        <div className= "formDiv">
           <form action="">
-            <div className={styles.formComponent}>
+            <div className="formComponent">
               <label >Full Name</label>
               <br />
               <input  type="text" placeholder="Full Name" />
             </div>
 
-            <div className={styles.formComponent}>
+            <div className="formComponent">
               <label >Email Address</label>
               <br />
               <input  type="text" placeholder="Work Email Address" />
             </div>
 
-            <div className={styles.formComponent}>
+            <div className="formComponent">
               <label >Company</label>
               <br />
               <input  type="text" placeholder="Company Name" />
             </div>
 
-            <div className={styles.formComponent}>
+            <div className="formComponent">
               <label >Website URL</label>
               <br />
               <input  type="text" placeholder="Company Website URL" />
             </div>
 
-            <div className={styles.formComponent}>
+            <div className="formComponent">
               <label >Password</label>
               <br />
               <input  type="text" placeholder="Password  ( minimum 8 characters )" />
             </div>
 
-            <input className={styles.nextSubmitBtn} type="submit" value="Get Started Now" />
+            <input className="nextSubmitBtn" type="submit" value="Get Started Now" />
            
           </form>
-        <p className={styles.terms}><span>By clicking "Get Started Now", you agree to CoSchedule’s </span><span className={styles.underlined}>terms of service, end user agreement, and privacy policy</span><span>; you are 16 years or older; and you will receive information from CoSchedule from which you can opt out at any time.</span></p>
+        <p className="terms"><span>By clicking "Get Started Now", you agree to CoSchedule’s </span><span className="underlined">terms of service, end user agreement, and privacy policy</span><span>; you are 16 years or older; and you will receive information from CoSchedule from which you can opt out at any time.</span></p>
         </div>
 
-        {/* <p className={styles.terms} style={{ marginTop:'60px'}}>
+        {/* <p className="terms" style={{ marginTop:'60px'}}>
           <span>© Copyright 2022, all rights reserved.  </span>
-          <span className={styles.underlined} > See our Terms & Policies.</span>
+          <span className="underlined} > See our Terms & Policies.</span>
         </p> */}
     </div>
   )
--- coschedule/src/pages/GetADemo.jsx
+++ coschedule/src/pages/GetADemo.jsx
@@ -1,54 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from "./getademo.module.css"
+import "../styles/getademo.css"
 
 const GetADemo = () => {
   return (
     <div>
-        <div className={styles.upperDiv}>
-            <Link to="/" ><img src="https://coschedule.com/img/cos-logo-full-color.svg" className={styles.cosLogo} alt="CoSchedule Logo" /></Link>
+        <div className="upperDiv">
+            <Link to="/" ><img src="https://coschedule.com/img/cos-logo-full-color.svg" className="cosLogo" alt="CoSchedule Logo" /></Link>
             <br />
-            <div className={styles.cosDemoTitle}>
-                <p className={styles.cosTitle}>See CoSchedule In Action</p>
-                <p className={styles.organizeCoschedule}>See how you can organize everything in one place with CoSchedule.</p>
+            <div className="cosDemoTitle">
+                <p className="cosTitle">See CoSchedule In Action</p>
+                <p className="organizeCoschedule">See how you can organize everything in one place with CoSchedule.</p>
             </div>
         </div>
 
-        <div className= {styles.formDiv}>
+        <div className="formDiv">
           <form action="">
-            <div className={styles.formComponent}>
+            <div className="formComponent">
               <label >Full Name</label>
               <br />
               <input  type="text" placeholder="Full Name" />
             </div>
 
-            <div className={styles.formComponent}>
+            <div className="formComponent">
               <label >Email Address</label>
               <br />
               <input  type="text" placeholder="Work Email Address" />
             </div>
 
-            <div className={styles.formComponent}>
+            <div className="formComponent">
               <label >Company</label>
               <br />
               <input  type="text" placeholder="Company Name" />
             </div>
 
-            <div className={styles.formComponent}>
+            <div className="formComponent">
               <label >Website URL</label>
               <br />
               <input  type="text" placeholder="Company Website URL" />
             </div>
 
-            <input className={styles.nextSubmitBtn} type="submit" value="Next" />
+            <input className="nextSubmitBtn" type="submit" value="Next" />
            
           </form>
-        <p className={styles.terms}><span>By signing up, you agree to CoSchedule’s </span><span className={styles.underlined}>terms of service, end user agreement, and privacy policy</span><span>; you are 16 years or older; and you will receive information from CoSchedule from which you can opt out at any time.</span></p>
+        <p className="terms"><span>By signing up, you agree to CoSchedule’s </span><span className="underlined" >terms of service, end user agreement, and privacy policy</span><span>; you are 16 years or older; and you will receive information from CoSchedule from which you can opt out at any time.</span></p>
         </div>
 
-        <p className={styles.terms} style={{ marginTop:'60px'}}>
+        <p className="terms"  style={{ marginTop:'60px'}}>
           <span>© Copyright 2022, all rights reserved.  </span>
-          <span className={styles.underlined} > See our Terms & Policies.</span>
+          <span className="underlined" > See our Terms & Policies.</span>
         </p>
     </div>
   )
