36e1ff7636a1a93e5ceac4949e2ca01728551574
Shreyansh12Gupta <shreyanshgupta148@gmail.com>
2022-06-16 12:50:25 +0530

    Update AllRoutes.jsx

--- coschedule/src/component/AllRoutes.jsx
+++ coschedule/src/component/AllRoutes.jsx
@@ -5,17 +5,19 @@ import Calender from '../pages/Calender';
 import { Box } from '@chakra-ui/react';
 import Footer from './Footer';
 import Pricing from '../pages/Pricing';
-import Home from "../component/Home"
+import Home from "../component/Home";
+
+// import Signup from "../pages/Signup";
 const AllRoutes = () => {
   return (
     <div>
       <Navbar />
       <Home/>
       <Routes>
+     
         <Route path='/Calender' element={<Calender />} />
         <Route path='/Pricing' element={<Pricing />} />
       </Routes>
-      <Box></Box>
       <h2 style={{ marginTop: '50px' }}>
         <Footer />
       </h2>
