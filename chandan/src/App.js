b3a15593d4d2f0adcb41eefccfbbde896f25ac1e
Goluchandan <99709975+Goluchandan@users.noreply.github.com>
2022-06-18 12:51:59 +0530

    updated

---
+++ chandan/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Customer from './Customer_Stories/Customer';
+import Actionable from './Actionable_Marketing/Actionable'
+function App() {
+  return (
+    <>
+    
+    <div className="App">
+      <Customer></Customer>
+    </div>
+
+    <div>
+      <Actionable />
+    </div>
+
+
+
+    </>
+  );
+}
+
+export default App;
