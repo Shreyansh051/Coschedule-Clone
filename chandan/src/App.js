95527e1679d76cfb18f0edc730686b85e26590eb
Goluchandan <99709975+Goluchandan@users.noreply.github.com>
2022-08-20 03:46:50 +0530

    update

--- chandan/src/App.js
+++ chandan/src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
-import Customer from './Customer_Stories/Customer';
+//import Customer from './Customer_Stories/Customer';
 import Actionable from './Actionable_Marketing/Actionable'
 function App() {
   return (
     <>
     
-    <div className="App">
+    {/* <div className="App">
       <Customer></Customer>
-    </div>
+    </div> */}
 
     <div>
       <Actionable />
